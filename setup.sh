f79bdedf3753c32cd648dbe4324631a0bd10bcac
”wmh11112345“ <373664138@qq.com>
2020-02-19 21:48:12 +0800

    add fi

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ elif which yum >/dev/null;then
 elif which brew > /dev/null ;then
         echo "you are using HomeBrew tool"
         brew install  vim git ctags vim-scripts vim-doc astyle
-
+fi
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 
 mv -f ~/vim  ~/vim_old
 cd ~/ && git clone https://github.com/wmh11112345/config_vim.git
 mv -f ~/.vim ~/.vim_old
-mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/.vim/.vimrc ~/
+mv -f ~/config_vim/.vimrc ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "正在安装bundle程序" > config_vim_log
@@ -24,3 +23,4 @@ echo "请耐心等待" >> config_vim_log
 vim config_vim_log -c "PlugInstall" -c "q" -c "q"
 rm -f  config_vim_log
 echo "安装完成"    
+
